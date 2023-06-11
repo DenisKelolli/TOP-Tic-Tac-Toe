237c50293de514b6a46bdc75f323663de8317336
Denis Kelolli <deniskelolli@gmail.com>
2023-06-11 12:20:53 -0400

    Added play again option

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TOP-Tic-Tac-Toe
 The Odin Project - Tic Tac Toe
+
+Tic-Tac-Toe Game built with HTML, CSS and Javascript. 
+
+![Alt text](<Tic-Tac-Toe Picture.JPG>)
--- script.js
+++ script.js
@@ -11,16 +11,20 @@ const counter = document.querySelector(".counter");
 counter.style.display = "none"; //Hides the counter from showing on the screen
 const winner = document.querySelector(".winner");
 const playAgainButton = document.querySelector(".play_again_button");
-
 const gridItems = document.querySelectorAll('.grid-item');
+
+let count = 0;
+
+
 
 
 const playGame = function() {
-  let count = 0;
+  
   gridItems.forEach((item) => {
     item.addEventListener('click', function(event) {
       // Check if the grid item already has a background color
       const hasBackgroundColor = event.target.style.backgroundColor !== '';
+
 
       if (!hasBackgroundColor) {
         count++;
@@ -43,8 +47,8 @@ const playGame = function() {
         (itemAtIndex1.style.backgroundColor === "green" && itemAtIndex5.style.backgroundColor === "green" && itemAtIndex9.style.backgroundColor === "green") ||
         (itemAtIndex3.style.backgroundColor === "green" && itemAtIndex5.style.backgroundColor === "green" && itemAtIndex7.style.backgroundColor === "green")
       ) {
-        winner.textContent = "Player wins!";
-      //Win conditions for computer (Red)
+        winner.textContent = "player1 wins!";
+      //Win conditions for player2 (Red)
       } else if (
         (itemAtIndex1.style.backgroundColor === "red" && itemAtIndex2.style.backgroundColor === "red" && itemAtIndex3.style.backgroundColor === "red") ||
         (itemAtIndex4.style.backgroundColor === "red" && itemAtIndex5.style.backgroundColor === "red" && itemAtIndex6.style.backgroundColor === "red") ||
@@ -55,12 +59,13 @@ const playGame = function() {
         (itemAtIndex1.style.backgroundColor === "red" && itemAtIndex5.style.backgroundColor === "red" && itemAtIndex9.style.backgroundColor === "red") ||
         (itemAtIndex3.style.backgroundColor === "red" && itemAtIndex5.style.backgroundColor === "red" && itemAtIndex7.style.backgroundColor === "red")
       ) {
-        winner.textContent = "Computer wins!";
+        winner.textContent = "player2 wins!";
       }
-      //Tie if either player has not win in 9 clicks
+      //Tie if either player has not won in 9 clicks
       else if(count === 9){
         winner.textContent = "Its a tie!";
       }
+
 
     });
   });
@@ -69,13 +74,15 @@ const playGame = function() {
 playGame();
 
 playAgainButton.addEventListener("click", function(){
-//Clears board once either the player or computer wins or there is a tie.
-if (winner.textContent === "Player wins!" || winner.textContent === "Computer wins!" || winner.textContent === "Its a tie!") {
-  gridItems.forEach((item) => {
-    item.style.backgroundColor = '';
-  });
-  winner.textContent = "";
-}
+//Clears board once either the player or player2 wins or there is a tie.
+  if (winner.textContent === "Player wins!" || winner.textContent === "player2 wins!" || winner.textContent === "Its a tie!") {
+    gridItems.forEach((item) => {
+      item.style.backgroundColor = '';
+    });
+    winner.textContent = "";
+    count = 0; //restart counter once the play again button is clicked.
+    counter.textContent = count;
+ }
 })
 
  
